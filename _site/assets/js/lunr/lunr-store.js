b67afc25153ac01dcb579133bf6742efb0809e49
tiru5 <tiru5@DESKTOP-94R1FL3>
2021-02-25 22:53:40 -0800

    trying something....

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "",
+        "excerpt":"Accurate :100:   💯 📏  📠     ","categories": [],
+        "tags": [],
+        "url": "/memes/best-js-meme/",
+        "teaser": "/assets/images/best-js-meme.png"
+      },{
+        "title": "",
+        "excerpt":"I legit laughed out loud, too true! :joy:     ","categories": [],
+        "tags": [],
+        "url": "/memes/framework-meme/",
+        "teaser": "/assets/images/new-javascript-framework.png"
+      },{
+        "title": "NF - Lost in the Moment",
+        "excerpt":"       This song means a lot to me :heart:  ","categories": [],
+        "tags": [],
+        "url": "/music/2021-02-25-NF-Lost-In-The-Moment/",
+        "teaser": "/assets/images/music/NF/NF_Lost_In_The_Moment.png"
+      },{
+        "title": "Chrome Extension",
+        "excerpt":":thinking: :bulb: :exclamation This was my first Chrome extension that I have ever implemented and released. It was such an awesome process, and I learned an insane amount! For instance, I learned how to work with and incorporate Chrome’s runtime API, and create the final bundle to upload to the...","categories": [],
+        "tags": [],
+        "url": "/portfolio/chrome-extension-project/",
+        "teaser": "/assets/images/amazon-portfolio/amazon_chrome_extension_icon_128x128.png"
+      },{
+        "title": "Updating my Website! 🚀",
+        "excerpt":"console.log('Hey peeps!👋 '); I am finally getting around to updating my website to make it look better, and to give it my own unique identity to it! 😊 To whomever is reading this – You are important, you have value, and you rock. Never stop being you, and keep on...","categories": [],
+        "tags": [],
+        "url": "/updating-website/",
+        "teaser": null
+      }]
